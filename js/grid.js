bc1222d83cfec341a27af7e9acd70cc09e6cc7dc
vuillaut <vuillaut@users.noreply.github.com>
2021-05-18 15:32:41 +0000

    deploy: 591c4f1132a5221ce41aa66382a13287497c9f63

---
+++ js/grid.js
@@ -0,0 +1,1 @@
+"use strict";function initGrid(){var n=$(".dream-grid").masonry({itemSelector:".dream-column"});n.imagesLoaded().progress(function(){return n.masonry("layout")}),window.hasTwitterEmbed&&window.twttr.ready(function(r){return r.events.bind("loaded",function(){return n.masonry("layout")})})}$(document).ready(initGrid);
