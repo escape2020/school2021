cb5cc684cda5b0f0cdaed35e411f32db2d135442
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2021-05-27 11:28:29 +0200

    Add workaround to all setups

--- packaging/eschool21_wheel_demo/setup.py
+++ packaging/eschool21_wheel_demo/setup.py
@@ -1,6 +1,10 @@
 from setuptools import setup
 from Cython.Build import cythonize
 
+# this is a workaround for an issue in pip that prevents editable installs
+# with --user, see https://github.com/pypa/pip/issues/7953
+import site, sys; site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
+
 setup(
     ext_modules=cythonize("eschool21_wheel_demo/fibonacci.pyx")
 )
