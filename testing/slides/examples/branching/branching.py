537520f2ce8776fbdb1476be6191c60b191d358e
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2021-05-28 10:58:22 +0200

    Add branching coverage

---
+++ testing/slides/examples/branching/branching.py
@@ -0,0 +1,12 @@
+def do_stuff():
+    print('stuff')
+
+
+def do_other_stuff():
+    print('other stuff')
+
+
+def branching(some_condition):
+    if some_condition is True:
+        do_stuff()
+    do_other_stuff()
