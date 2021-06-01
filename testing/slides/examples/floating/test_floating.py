568cc6eaa8b63473b30f39de0bee0c89bdf7ab2c
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2021-06-01 11:49:08 +0200

    Also run example tests in ci

--- testing/slides/examples/floating/test_floating.py
+++ testing/slides/examples/floating/test_floating.py
@@ -1,6 +1,6 @@
 import pytest
 
-def test_addition():
+def test_addition_naive():
     assert 0.1 + 0.2 == 0.3
 
 def test_addition_correct():
--- testing/slides/examples/test_marks.py
+++ testing/slides/examples/test_marks.py
@@ -3,7 +3,7 @@ import time
 
 @pytest.mark.slow
 def test_slow():
-    time.sleep(5)
+    time.sleep(2)
     assert 1 + 1 == 2
 
 def test_fast():
